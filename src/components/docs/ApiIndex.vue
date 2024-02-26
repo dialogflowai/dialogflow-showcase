e621ad27733c9161aa5ccc780592808677b31c64
dialogflow <dialogflow@yeah.net>
2024-02-26 17:21:26 +0800

    New documentations

---
+++ src/components/docs/ApiIndex.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ApiJavaScript from './ApiJavaScript.vue';
+import ApiPython from './ApiPython.vue';
+import ApiJava from './ApiJava.vue';
+const currentDoc = ref('ApiJavaScript')
+const docs = {
+    ApiJavaScript,
+    ApiPython,
+    ApiJava,
+}
+
+</script>
+<template>
+    <el-row>
+        <el-col :span="5">
+            <div class="item" @click="currentDoc = 'ApiJavaScript'">JavaScript</div>
+            <div class="item" @click="currentDoc = 'ApiPython'">Python</div>
+            <div class="item" @click="currentDoc = 'ApiJava'">Java</div>
+        </el-col>
+        <el-col :span="19" style="padding-left: 6px;">
+            <keep-alive>
+                <component :is="docs[currentDoc]"></component>
+            </keep-alive>
+        </el-col>
+    </el-row>
+</template>
--- src/components/docs/Index.vue
+++ src/components/docs/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, ref } from "vue";
 import { useRouter } from 'vue-router';
-import Api from './Api.vue';
+import ApiIndex from './ApiIndex.vue';
 import FlowEditDoc from './FlowEdit.vue';
 import HowToUseDoc from './HowToUse.vue';
 import KnowledgeDoc from './KmsDoc.vue';
@@ -13,9 +13,9 @@ import BiChatSquareDots from '~icons/bi/chat-square-dots'
 import SolarDownloadOutline from '~icons/solar/download-outline'
 const { t, tm, rt } = useI18n();
 const router = useRouter();
-const currentDoc = ref('Api')
+const currentDoc = ref('ApiIndex')
 const docs = {
-    Api,
+    ApiIndex,
     FlowEditDoc,
     HowToUseDoc,
     KnowledgeDoc,
@@ -30,6 +30,9 @@ const menuItemClassObject = computed(() => ({
     item: currentDoc.value === '',
     itemSelected: false,
 }))
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
+}
 </script>
 <style scoped>
 .title {
@@ -59,39 +62,24 @@ const menuItemClassObject = computed(() => ({
         </template>
     </el-page-header>
     <p></p>
-    <el-row>
-        <el-col :span="5">
-            <p>
-            <div class="title">
-                <el-icon size="20">
-                    <BiChatSquareDots />
-                </el-icon>
-                Dialog flow
-            </div>
-            <div class="item" @click="currentDoc = 'HowToUseDoc'">How to create a dialog flow?</div>
-            <div class="item" @click="currentDoc = 'NodesIntro'">Nodes introduction</div>
-            <div class="item" @click="currentDoc = 'Api'">Program interface integration guide</div>
-            </p>
-            <!-- <p>
-            <div class="title">
-                <el-icon size="20">
-                    <Orange />
-                </el-icon>Intents (WIP)
-            </div>
-            </p> -->
-            <p>
-            <div class="title">
-                <el-icon size="20">
-                    <SolarDownloadOutline />
-                </el-icon>Variables
-            </div>
-            <div class="item" @click="currentDoc = 'VariableHttpDoc'">How to retrieve data from HTTP?</div>
-            </p>
-        </el-col>
-        <el-col :span="19" style="padding-left: 6px;">
-            <keep-alive>
-                <component :is="docs[currentDoc]"></component>
-            </keep-alive>
-        </el-col>
-    </el-row>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-sub-menu index="2">
+            <template #title>Workspace</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-sub-menu index="2-4">
+                <template #title>item four</template>
+                <el-menu-item index="2-4-1">item one</el-menu-item>
+                <el-menu-item index="2-4-2">item two</el-menu-item>
+                <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4">Orders</el-menu-item>
+    </el-menu>
+    <keep-alive>
+        <component :is="docs[currentDoc]"></component>
+    </keep-alive>
 </template>
