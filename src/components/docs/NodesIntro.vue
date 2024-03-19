a1275e0258cfc33febddbb4b266a4855fb984829
dialogflow <dialogflow@yeah.net>
2024-03-19 18:07:07 +0800

    Changed description

--- src/components/docs/NodesIntro.vue
+++ src/components/docs/NodesIntro.vue
@@ -125,7 +125,7 @@
       <span>Create your own node</span>
       <div class="desc">
         Use your imagination and create your own node<br />
-        For example, a node that assigns a value to a variable,<br />
+        For example, a Slack/Discord bot node,<br />
         or a node that uses ChatGPT, exits when the user enters specific characters.<br />
         <br />
         If you have any good ideas or needs, you can also submit them to <a href="https://github.com/dialogflowchatbot/dialogflow/discussions">Discussions</a> on Github
