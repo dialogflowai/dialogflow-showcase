3ed9b1f39db0ca603912d630f2771580a4a39553
dialogflowchatbot <dialogflow@yeah.net>
2024-12-26 23:34:43 +0800

    Added description of some nodes

--- src/components/docs/node/KnowledgeBaseAnswerNode.vue
+++ src/components/docs/node/KnowledgeBaseAnswerNode.vue
@@ -1,28 +1,12 @@
 <script setup>
 const tableData = [
     {
-        name: 'Chat model',
-        description: 'The model using by this node, you can change this on settings page.',
+        name: 'Knowledge recall thresholds',
+        description: 'Find similarities between the user\'s input and the data in the knowledge base. The higher the number, the greater the match for the knowledge found.',
     },
     {
-        name: 'Prompt',
-        description: 'Define LLM behavior.',
-    },
-    {
-        name: 'Context length',
-        description: 'How many chat history will be appended to prompt.',
-    },
-    {
-        name: 'Exit condition',
-        description: 'Define how to exit this chat node. There are three options: Intent (Exit when matching on intent), Special input (Once user input a special input then exit this node), Max chat times (Stay in this node until reach max chat times).',
-    },
-    {
-        name: 'Timeout',
-        description: 'You can override HTTP connection settings.',
-    },
-    {
-        name: 'When timeout then',
-        description: 'There are three options: Goto next node (if HTTP request timeout, goto next node), Response an alternate text (return another sentence instead of LLM response), Do nothing(Nothing will be returned to user and then waiting user\'s next inputs).',
+        name: 'Retrieve answer from',
+        description: 'Queries from Q&A and document libraries are supported. You can set the query from the Q&A library or the document library first, or only the Q&A library or the document library query.',
     },
 ];
 </script>
--- src/components/docs/node/TheEndNode.vue
+++ src/components/docs/node/TheEndNode.vue
@@ -1,28 +1,8 @@
 <script setup>
 const tableData = [
     {
-        name: 'Chat model',
-        description: 'The model using by this node, you can change this on settings page.',
-    },
-    {
-        name: 'Prompt',
-        description: 'Define LLM behavior.',
-    },
-    {
-        name: 'Context length',
-        description: 'How many chat history will be appended to prompt.',
-    },
-    {
-        name: 'Exit condition',
-        description: 'Define how to exit this chat node. There are three options: Intent (Exit when matching on intent), Special input (Once user input a special input then exit this node), Max chat times (Stay in this node until reach max chat times).',
-    },
-    {
-        name: 'Timeout',
-        description: 'You can override HTTP connection settings.',
-    },
-    {
-        name: 'When timeout then',
-        description: 'There are three options: Goto next node (if HTTP request timeout, goto next node), Response an alternate text (return another sentence instead of LLM response), Do nothing(Nothing will be returned to user and then waiting user\'s next inputs).',
+        name: '',
+        description: 'This node can only be placed at the end of the dialog flow, indicating the end point of the process.',
     },
 ];
 </script>
